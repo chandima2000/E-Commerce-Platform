bdaf77af67bbc652d1a262cdd56ce2afb20835e1
chandima2000 <hychandima2000@gmail.com>
2025-01-31 13:47:50 +0530

    Initial project setup for order-service

---
+++ order-service/src/test/java/org/chandima/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package org.chandima.orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
