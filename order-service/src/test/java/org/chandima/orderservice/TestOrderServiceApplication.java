447f394ea7cf2a15bcccfb3e4fa0575ea0e4b36d
chandima2000 <hychandima2000@gmail.com>
2025-02-01 10:48:13 +0530

    add some clarification comments

--- order-service/src/test/java/org/chandima/orderservice/TestOrderServiceApplication.java
+++ order-service/src/test/java/org/chandima/orderservice/TestOrderServiceApplication.java
@@ -7,8 +7,10 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class TestOrderServiceApplication {
-
-
+    
+    // When is This Needed?
+    // For integration testing, where you need a real database instance but don't want to use a real MySQL server.
+    // Ensures that tests run in an isolated environment
     @Bean
     @ServiceConnection
     MySQLContainer<?> mysqlContainer() {
