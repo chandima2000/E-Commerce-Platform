94a65d32f4670bc843eecc92c56a19bff80f3672
chandima2000 <hychandima2000@gmail.com>
2025-02-01 18:47:38 +0530

    run integration test for inventory-service

--- inventory-service/src/test/java/org/chandima/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/org/chandima/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package org.chandima.inventoryservice;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI="http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldReadInventory() {
+
+       boolean positiveResponse = RestAssured.given()
+                .when()
+                .get("/api/inventory?skuCode=iphone_15&quantity=1")   // quantity is less than 100, return True
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response().as(Boolean.class);
+        Assertions.assertTrue(positiveResponse);
+
+
+        boolean negativeResponse = RestAssured.given()
+                .when()
+                .get("/api/inventory?skuCode=iphone_15&quantity=1000") // quantity is greater than 100, return false
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response().as(Boolean.class);
+        Assertions.assertFalse(negativeResponse);
+
     }
 
 }
