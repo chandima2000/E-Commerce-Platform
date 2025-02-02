5992af24776ba230e89281abff3e888de7e35d33
chandima2000 <hychandima2000@gmail.com>
2025-02-02 10:09:39 +0530

    establish inter service communication between order-service and inventory-service

--- order-service/src/main/java/org/chandima/orderservice/service/OrderService.java
+++ order-service/src/main/java/org/chandima/orderservice/service/OrderService.java
@@ -1,11 +1,13 @@
 package org.chandima.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.chandima.orderservice.client.InventoryClient;
 import org.chandima.orderservice.dto.OrderRequst;
 import org.chandima.orderservice.model.Order;
 import org.chandima.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
+import javax.management.RuntimeErrorException;
 import java.util.UUID;
 
 @Service
@@ -14,25 +16,34 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequst orderRequst) {
 
-//        Order order = new Order();
-//        order.setOrderNumber(UUID.randomUUID().toString());
-//        order.setPrice(orderRequst.price());
-//        order.setSkuCode(orderRequst.skuCode());
-//        order.setQuantity(orderRequst.quantity());
-//        orderRepository.save(order);
+        boolean isStockAvailable = inventoryClient.isInStock(orderRequst.skuCode(), orderRequst.quantity());
 
+        //        Order order = new Order();
+        //        order.setOrderNumber(UUID.randomUUID().toString());
+        //        order.setPrice(orderRequst.price());
+        //        order.setSkuCode(orderRequst.skuCode());
+        //        order.setQuantity(orderRequst.quantity());
+        //        orderRepository.save(order);
 
-       // use the Builder Pattern
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequst.price())
-                .skuCode(orderRequst.skuCode())
-                .quantity(orderRequst.quantity())
-                .build();
+        if (isStockAvailable) {
+            // use the Builder Pattern
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequst.price())
+                    .skuCode(orderRequst.skuCode())
+                    .quantity(orderRequst.quantity())
+                    .build();
 
-        orderRepository.save(order);
+            orderRepository.save(order);
+        }
+        else{
+            throw new RuntimeException("Product with skuCode "+orderRequst.skuCode()+" is not available");
+        }
+
 
     }
 }
