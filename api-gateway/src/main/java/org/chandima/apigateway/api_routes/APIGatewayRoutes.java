e647e8fb7c03b6c229f2f290d1838dc2aac2de09
chandima2000 <hychandima2000@gmail.com>
2025-02-03 11:30:40 +0530

    implement api-gateway routes for microservices

---
+++ api-gateway/src/main/java/org/chandima/apigateway/api_routes/APIGatewayRoutes.java
@@ -0,0 +1,37 @@
+package org.chandima.apigateway.api_routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class APIGatewayRoutes {
+
+    // Functional Endpoints Approach
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoutes(){
+        return GatewayRouterFunctions.route("product-service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoutes(){
+        return GatewayRouterFunctions.route("order-service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoutes(){
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+}
