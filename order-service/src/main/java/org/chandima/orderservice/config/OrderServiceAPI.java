59f88d9d455fbbcd0dc73a059b19e77a00f4346f
chandima2000 <hychandima2000@gmail.com>
2025-02-04 17:41:34 +0530

    create API documentation for product-service in Swagger-ui and JSON format

---
+++ order-service/src/main/java/org/chandima/orderservice/config/OrderServiceAPI.java
@@ -0,0 +1,25 @@
+package org.chandima.orderservice.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderServiceAPI {
+
+    // Documenting order-service REST API.
+    @Bean
+    public OpenAPI oderServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service Api")
+                        .description("This is the Order Service API.")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
