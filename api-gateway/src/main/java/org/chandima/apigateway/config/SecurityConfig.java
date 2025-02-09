52751b24cf000b71c980cf6244963205928f7e4e
chandima2000 <hychandima2000@gmail.com>
2025-02-09 23:55:03 +0530

    add CORS origin configurations to api-gateway

--- api-gateway/src/main/java/org/chandima/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/chandima/apigateway/config/SecurityConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 
 
 @Configuration
@@ -22,8 +26,20 @@ public class SecurityConfig {
                         .requestMatchers(freeResourceUrls)
                         .permitAll()
                         .anyRequest().authenticated())
+                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
                       return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
