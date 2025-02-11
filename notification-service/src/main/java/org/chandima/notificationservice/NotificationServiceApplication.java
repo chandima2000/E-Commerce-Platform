a36e63637a87f1cb990a7e399f27024335668f2d
chandima2000 <hychandima2000@gmail.com>
2025-02-11 16:26:45 +0530

    create notification-service project

---
+++ notification-service/src/main/java/org/chandima/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.chandima.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
